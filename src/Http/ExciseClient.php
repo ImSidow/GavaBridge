981682fdbcedb3468454cb6d7040f2d1ff320eb1
ImSidow <sidow.code@gmail.com>
2025-08-17 15:28:44 +0300

    add: Http/ExciseClient.php

---
+++ src/Http/ExciseClient.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace GavaConnect\Kra\Http;
+
+class ExciseClient extends BaseClient
+{
+    public function checkByNumber(string $certificateNumber): array
+    {
+        $payload = ['certificate_number' => $certificateNumber];
+        $res = $this->http()->get($this->endpoint('excise_license'), $payload);
+        return $this->unwrap($res);
+    }
+}
