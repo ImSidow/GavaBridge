5c9c085d58c09c0fb54c9e5a09e1b80a06fba2b4
ImSidow <sidow.code@gmail.com>
2025-08-17 15:25:28 +0300

    add: Http/TccClient.php

---
+++ src/Http/TccClient.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace GavaConnect\Kra\Http;
+
+class TccClient extends BaseClient
+{
+    public function validate(string $pin, string $tccNumber): array
+    {
+        $payload = ['pin' => $pin, 'tcc_number' => $tccNumber];
+        $res = $this->http()->post($this->endpoint('tcc_validate'), $payload);
+        return $this->unwrap($res);
+    }
+}
