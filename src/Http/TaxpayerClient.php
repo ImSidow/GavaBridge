a313938bd0e5b1f3a0ed3f271502a947a7066c14
ImSidow <sidow.code@gmail.com>
2025-08-17 15:33:33 +0300

    add: Http/TaxpayerClient.php

---
+++ src/Http/TaxpayerClient.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace GavaConnect\Kra\Http;
+
+class TaxpayerClient extends BaseClient
+{
+    public function obligations(string $pin): array
+    {
+        $payload = ['pin' => $pin];
+        $res = $this->http()->get($this->endpoint('obligations'), $payload);
+        return $this->unwrap($res);
+    }
+
+    public function liabilities(string $pin): array
+    {
+        $payload = ['pin' => $pin];
+        $res = $this->http()->get($this->endpoint('liabilities'), $payload);
+        return $this->unwrap($res);
+    }
+}
