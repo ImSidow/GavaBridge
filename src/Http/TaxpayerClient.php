328e7b62383f1a2a26e5020caa2cd112f60635ca
ImSidow <sidow.code@gmail.com>
2025-08-21 16:51:46 +0300

    modified: some client url methods

--- src/Http/TaxpayerClient.php
+++ src/Http/TaxpayerClient.php
@@ -7,14 +7,14 @@ class TaxpayerClient extends BaseClient
     public function obligations(string $pin): array
     {
         $payload = ['taxPayerPin' => $pin];
-        $res = $this->get($this->endpoint('obligations'), $payload);
+        $res = $this->post($this->endpoint('obligations'), $payload);
         return $this->unwrap($res);
     }
 
     public function liabilities(string $pin, string $obligationId): array
     {
         $payload = ['taxPayerPin' => $pin, 'obligationId' => $obligationId];
-        $res = $this->get($this->endpoint('liabilities'), $payload);
+        $res = $this->post($this->endpoint('liabilities'), $payload);
         return $this->unwrap($res);
     }
 }
